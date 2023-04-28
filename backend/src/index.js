ead062863d1aadd878a82f5b5b3e5165fd266ae0
Bablu <bk559722@gmail.com>
2023-04-28 12:24:54 +0530

    add dynamic PORT

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,13 +4,13 @@ require("./db/conn")
 const clientRouter = require("./routers/clientData")
 
 const app = express()
-const port = 7000
+const PORT = process.env.PORT || 7000
 app.use(express.json())
 app.use(clientRouter)
 
 
 
-app.listen(port, () => {
-    console.group(`this app listning on  port no. ${port}`)
+app.listen(PORT, () => {
+    console.group(`this app listning on  port no. ${PORT}`)
 })
 
