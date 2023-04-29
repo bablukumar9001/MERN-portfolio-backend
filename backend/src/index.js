d7707accb9fc73409f4436ee2170150b1d2c15a4
Bablu <bk559722@gmail.com>
2023-04-29 22:52:34 +0530

    change cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 require('dotenv').config()
 require("./db/conn")
 const clientRouter = require("./routers/clientData")
+const cors = require("cors");
+
+app.use(cors());
 
 const app = express()
 const PORT = process.env.PORT || 7000
