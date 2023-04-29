d38d5fe3d7c7440470e14feaa092640d7003e31e
Bablu <bk559722@gmail.com>
2023-04-29 22:57:17 +0530

    new commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,13 @@ require("./db/conn")
 const clientRouter = require("./routers/clientData")
 const cors = require("cors");
 
-app.use(cors());
+
 
 const app = express()
 const PORT = process.env.PORT || 7000
 app.use(express.json())
 app.use(clientRouter)
+app.use(cors());
 
 
 
