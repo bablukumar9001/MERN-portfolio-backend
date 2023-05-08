2f84a0c94c434f2baac6cfd1e049335590c2fab9
Bablu <bk559722@gmail.com>
2023-05-08 17:11:30 +0530

    change

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('dotenv').config()
 require("./db/conn")
 const clientRouter = require("./routers/clientData")
-// const cors = require("cors");
+const cors = require("cors");
 
 
 
@@ -10,8 +10,15 @@ const app = express()
 const PORT = process.env.PORT || 7000
 app.use(express.json())
 app.use(clientRouter)
-// app.use(cors());
-
+const cors = require('cors');
+const corsOpts = {
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'HEAD', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+    exposedHeaders: ['Content-Type']
+};
+app.use(cors(corsOpts));
 
 
 app.listen(PORT, () => {
