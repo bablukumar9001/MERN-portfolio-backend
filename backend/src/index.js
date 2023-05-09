e4a2085c385328a7a05d3cbe399db88ab3fa6248
Bablu <bk559722@gmail.com>
2023-05-09 14:48:06 +0530

    comit

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('dotenv').config()
 require("./db/conn")
 const clientRouter = require("./routers/clientData")
-const cors = require("cors");
+// const cors = require("cors");
 
 
 
@@ -10,12 +10,12 @@ const app = express()
 const PORT = process.env.PORT || 7000
 app.use(express.json())
 app.use(clientRouter)
-// const cors = require('cors');
-const corsOpts = {
-    origin: 'https://bablukumar.onrender.com/',
-    credentials: true,
-};
-app.use(cors(corsOpts));
+// // const cors = require('cors');
+// const corsOpts = {
+//     origin: 'https://bablukumar.onrender.com/',
+//     credentials: true,
+// };
+// app.use(cors(corsOpts));
 
 
 app.listen(PORT, () => {
