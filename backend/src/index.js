df7ffbd9d356054d502a543628d2a4833dca7f55
Bablu <bk559722@gmail.com>
2023-05-11 13:38:30 +0530

    some change

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,12 +10,15 @@ const app = express()
 const PORT = process.env.PORT || 7000
 app.use(express.json())
 app.use(clientRouter)
-// // const cors = require('cors');
-// const corsOpts = {
-//     origin: 'https://bablukumar.onrender.com/',
-//     credentials: true,
-// };
-// app.use(cors(corsOpts));
+// const cors = require('cors');
+const corsOpts = {
+    origin: 'https://bablukumar.onrender.com/',
+    credentials: true,
+    allowedHeaders: "*",
+    allowMethods: "*",
+
+};
+app.use(cors(corsOpts));
 
 
 app.listen(PORT, () => {
